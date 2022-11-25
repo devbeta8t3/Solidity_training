a3e3889ebb740f788013e7176a274d9089b5e2b7
devbeta8t3 <sehoan30@gmail.com>
2022-11-25 14:13:24 +0900

    training for mapping and struct

--- TestMapping.sol
+++ TestMapping.sol
@@ -3,9 +3,23 @@ pragma solidity ^0.8.7;
 
 contract TestMapping {
 
-    mapping (uint => bool) public m1;
+    bool a1 = false;
+    bool a2 = false;
+    bool a3 = false;
+
+    mapping (uint => bool) public m1;   // 외부에서 접근 가능 (유저)
+    mapping (address => bool) public m2;
 
     function setValueM1(uint _key) public {
         m1[_key] = true;
     }
+
+    function setA1ToTrue() public {
+        a1 = true;
+    }
+
+    function setM2ToTrue() public {
+        m2[msg.sender] = true;
+    }
+
 }
--- TestStruct.sol
+++ TestStruct.sol
@@ -1,3 +1,67 @@
 // SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.7;
 
+contract TestStruct {
+
+    struct Transaction {
+        uint amount;
+        uint timestamp;
+    }
+
+    struct Balance {
+        uint totalBalance;
+
+        uint numDeposits;
+        mapping(uint => Transaction) deposits;
+        
+        uint numWithdrawals;
+        mapping(uint => Transaction) withdrawals;
+    }
+
+    //mapping (address => uint) public balanceReceived;
+    mapping (address => Balance) public balanceReceived;
+
+    function getBalance() public view returns(uint) {
+        return address(this).balance;
+    }
+
+    function getBalanceAtAddress(address _addr) public view returns(uint) {
+        return balanceReceived[_addr].totalBalance;
+    }
+
+    function depositMoney() public payable {
+        //balanceReceived[msg.sender] += msg.value;
+        balanceReceived[msg.sender].totalBalance += msg.value;
+
+        //Payment memory payment = Payment(msg.value, now);
+        Transaction memory deposit = Transaction(msg.value, block.timestamp);
+
+        balanceReceived[msg.sender].deposits[balanceReceived[msg.sender].numDeposits] = deposit;
+        balanceReceived[msg.sender].numDeposits++;
+
+    }
+
+    function withdrawMoney(address payable _to, uint _amount) public {
+        //require(balanceReceived[msg.sender] >= _amount, "not enough money");
+        require(balanceReceived[msg.sender].totalBalance >= _amount, "not enough money");
+        
+        //balanceReceived[msg.sender] -= _amount;
+        balanceReceived[msg.sender].totalBalance -= _amount;
+
+        Transaction memory withdrawal = Transaction(_amount, block.timestamp);
+        balanceReceived[msg.sender].withdrawals[balanceReceived[msg.sender].numWithdrawals] = withdrawal;
+        balanceReceived[msg.sender].numWithdrawals;
+
+        _to.transfer(_amount);
+    }
+
+    function withdrawAllMoney(address payable _to) public {
+        //uint balanceToSend = balanceReceived[msg.sender];
+        uint balanceToSend = balanceReceived[msg.sender].totalBalance;
+
+        //balanceReceived[msg.sender] = 0;
+        balanceReceived[msg.sender].totalBalance = 0;
+        
+        _to.transfer(balanceToSend);
+    }
+}
